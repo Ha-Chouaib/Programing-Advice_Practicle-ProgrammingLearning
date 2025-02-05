63148ce66b0ebc52755e7471475fa83f955c6156
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-05 07:51:27 -0500

    C# Synthax|-->[output]-(01_WirteLine)

---
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/01_Output/01_WriteLine/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bismi ALLAH");
+            Console.WriteLine("Welcom To .NET World");
+            Console.WriteLine("With C# Language");
+            Console.WriteLine("Easy Logic 1+1= "+(1+1));
+
+        }
+    }
+}
