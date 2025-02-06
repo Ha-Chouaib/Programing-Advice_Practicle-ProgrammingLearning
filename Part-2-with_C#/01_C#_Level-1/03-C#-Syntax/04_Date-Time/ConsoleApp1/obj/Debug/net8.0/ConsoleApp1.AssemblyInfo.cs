40e0b7ca926afe1195abd9acdc396cd06ce26684
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-06 12:32:26 -0500

    C# Synthax|-->[Date Time]-(Operators)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31f77385465e55135100cb0378f68be1af5baa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83fc347857aa4f81fe903a23440bdca3572cc1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
