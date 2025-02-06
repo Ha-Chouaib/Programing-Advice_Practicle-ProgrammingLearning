538fb88049e11a3c716ed1b36b4240e482f9465c
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-06 10:46:34 -0500

    C# Synthax|-->[Date Time]-(Date Time Static Field)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7371838958aecfe7d35675af3eaa577f4d9e8c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8616252339b3a729cb4b882deee633877a1e9384")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
