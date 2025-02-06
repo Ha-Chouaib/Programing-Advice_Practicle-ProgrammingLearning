5787897670194bd7e5128e1c5806f7db39d2cf25
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-06 10:05:57 -0500

    C# Synthax|-->[Date Time]-(Get Current Date)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/Program.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace Main
     {
         static void Main(string[] args)
         {
+            //............Set Date....................
             DateTime DT1= new DateTime();// assign the default value (01/01/0001 00:00:00)
             DateTime DT2= new DateTime(2002,3,21);//assign Year, Month, Day
             DateTime DT3= new DateTime(2002,2,22,12,30,45);//Assign Year/Month/Day/Hour/Min/Sec
@@ -15,6 +16,11 @@ namespace Main
             Console.WriteLine(DT2);
             Console.WriteLine(DT3);
             Console.WriteLine(DT4);
+
+            //............Get Current Date............
+            DateTime dtn=new DateTime();
+            dtn=DateTime.Now;
+            Console.WriteLine("The Current Date: {0}",dtn);
 
 
         }
