83fc347857aa4f81fe903a23440bdca3572cc1d3
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-06 12:09:38 -0500

    C# Synthax|-->[Date Time]--(Sub Tow Dates)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/03_DataTypes/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/03_DataTypes/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b65b5c0d96b31dadd6b87434c41a02a4eaac2b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31f77385465e55135100cb0378f68be1af5baa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/04_Date-Time/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538fb88049e11a3c716ed1b36b4240e482f9465c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31f77385465e55135100cb0378f68be1af5baa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
