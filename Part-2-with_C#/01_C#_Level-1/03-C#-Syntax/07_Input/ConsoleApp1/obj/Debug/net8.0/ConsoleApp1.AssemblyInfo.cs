d11eddb604c97ef6fd9c70118dadfbb1450bf2e0
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-07 09:34:50 -0500

    C# Synthax|-->[Input]--(Read The Other Data Types)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/07_Input/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/07_Input/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679267e44d83908ee8d7c2eec11534983453487a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4d55bb444f0ae0b55d374d3f953b34ff6c8085")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
