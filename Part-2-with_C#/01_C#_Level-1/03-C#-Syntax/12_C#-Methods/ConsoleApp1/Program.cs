084aa0ffce94e289c169785bbcadaa9b559f45c9
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-07 14:07:32 -0500

    C# Synthax|-->[C# Methods]--(Named Argument)

--- Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/12_C#-Methods/ConsoleApp1/Program.cs
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/12_C#-Methods/ConsoleApp1/Program.cs
@@ -19,12 +19,16 @@ namespace Main
         {
             Console.WriteLine("Name= {0} , Age= {1}",Name,Age);
         }
+        static void MyMethod(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest child is: " + child3);
+        }
         static void Main(string[] args)
             {
 
             PrintMyName();
             PrintMyInfo(GetMyName(),45);
-
+            MyMethod(child3: "Omar", child1: "Saqer", child2: "Hamza");
             }
         }
     }
