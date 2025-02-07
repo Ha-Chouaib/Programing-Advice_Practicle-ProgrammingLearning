5ac1d11f07607c665f55ecc1221f144b831afb9f
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-07 09:26:47 -0500

    C# Synthax|-->[Input]--(ReadLine())

---
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/07_Input/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //.............................ReadLine()..........................
+            //ReadLine() Method Allways Reads String Values Equivalent to getline() in C++
+
+            Console.WriteLine("Please Enter Full Name?");
+            string FullName = Console.ReadLine();
+            Console.WriteLine($"\nFullName: {FullName}");
+            
+        }
+    }
+}
