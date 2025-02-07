cae63463caf67cac5e3ba1f0e995cd709b50dbe8
Chouaib Hadadi <Chouaib.Hadadi01@gmail.com>
2025-02-07 13:08:36 -0500

    C# Synthax|-->[C# Methods]--(Simple Method && with Parameters)

---
+++ Part-2-with_C#/01_C#_Level-1/03-C#-Syntax/12_C#-Methods/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Main
+    {
+        internal class Program
+        {
+
+        static void PrintMyName()
+        {
+            Console.WriteLine("Chouaib Hadadi");
+        }
+        static void PrintMyInfo(string Name,byte Age)
+        {
+            Console.WriteLine("Name= {0} , Age= {1}",Name,Age);
+        }
+        static void Main(string[] args)
+            {
+
+            PrintMyName();
+            PrintMyInfo("Karim-Hilmi",45);
+
+            }
+        }
+    }
