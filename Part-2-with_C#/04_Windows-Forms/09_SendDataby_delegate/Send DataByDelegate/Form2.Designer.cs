3c7a583a85d411fadbd3c149853ee0612826bda0
Chouaib.Hadadi01 <Chouaib.Hadadi01@gmail.com>
2025-04-11 03:18:34 +0100

    Dealing With User Control && Delegate

---
+++ Part-2-with_C#/04_Windows-Forms/09_SendDataby_delegate/Send DataByDelegate/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Send_DataByDelegate
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendBackData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendBackData
+            // 
+            this.btnSendBackData.Location = new System.Drawing.Point(439, 178);
+            this.btnSendBackData.Name = "btnSendBackData";
+            this.btnSendBackData.Size = new System.Drawing.Size(97, 23);
+            this.btnSendBackData.TabIndex = 0;
+            this.btnSendBackData.Text = "SendBackData";
+            this.btnSendBackData.UseVisualStyleBackColor = true;
+            this.btnSendBackData.Click += new System.EventHandler(this.btnSendBackData_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSendBackData);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendBackData;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Part-2-with_C#/04_Windows-Forms/09_SendDataby_delegate/Send DataByDelegate/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Send_DataByDelegate
+{
+    public partial class Form2: Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void SendBackDataEventHandler(object sender, int PersonID);
+
+        public event SendBackDataEventHandler SendDataBack; 
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSendBackData_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(textBox1.Text,out int PersonID))
+            {
+                SendDataBack?.Invoke(this, PersonID);
+                this.Close();
+            }
+        }
+    }
+}
