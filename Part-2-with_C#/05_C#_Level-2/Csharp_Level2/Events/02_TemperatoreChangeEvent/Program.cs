452368c1ec2fb037b3c682ee78308a88a244759a
Chouaib.Hadadi01 <Chouaib.Hadadi01@gmail.com>
2025-06-14 04:14:10 +0100

    C# Level 2 Pick The Programing To The Next Level [Events == Publisher Subscriber Design Patterns]

---
+++ Part-2-with_C#/05_C#_Level-2/Csharp_Level2/Events/02_TemperatoreChangeEvent/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatoreChangeEvent
+{
+    public class TemperatorChangeEventArgs : EventArgs
+    {
+        public double CurrentTemprature { get;}
+        public double OldTemperature { get; }
+        public double Difference { get; }
+
+        public TemperatorChangeEventArgs(double CurrentTemprature, double OldTemperator)
+        {
+            this.CurrentTemprature = CurrentTemprature;
+            this.OldTemperature = OldTemperator;
+            this.Difference = CurrentTemprature - OldTemperator;
+        }
+    }
+
+    public class Thermostat
+    {
+        public event EventHandler<TemperatorChangeEventArgs> TempratureChanged;
+
+        private double OldTemprature, CurrentTeprature;
+
+        public void SetTemprature(double Temprature)
+        {
+            if (Temprature != CurrentTeprature)
+            {
+                OldTemprature = CurrentTeprature;
+                CurrentTeprature = Temprature;
+
+                OnTemperatureChanged(CurrentTeprature, OldTemprature);
+            }
+        }
+        private void OnTemperatureChanged(double CurrentTemprature, double OldTempratue)
+        {
+            OnTemperatureChanged(new TemperatorChangeEventArgs(CurrentTemprature, OldTempratue));
+        }
+
+        protected virtual void OnTemperatureChanged(TemperatorChangeEventArgs e)
+        {
+            TempratureChanged?.Invoke(this, e);
+        }
+
+    }
+
+    public class Display
+    {
+        public void Subscribe(Thermostat thermostat)
+        {
+            thermostat.TempratureChanged += TemperatorChangeHandler;
+        }
+
+        public void TemperatorChangeHandler(object sender, TemperatorChangeEventArgs e)
+        {
+            Console.WriteLine("Temperatue Changes\n\n");
+            Console.WriteLine($"Temperatue Changed From: << {e.OldTemperature} C >>\n");
+            Console.WriteLine($"Temperatue Changed To: << {e.CurrentTemprature} C >>\n");
+            Console.WriteLine($"Temperatue Difference: < {e.Difference} C >\n");
+        }
+        public class Program 
+        {
+            public static void Main(string[] args)
+            {
+                Thermostat thermostat = new Thermostat();
+                Display display = new Display();
+
+                display.Subscribe(thermostat);
+
+                thermostat.SetTemprature(32);
+                thermostat.SetTemprature(2);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Part-2-with_C#/05_C#_Level-2/Csharp_Level2/ctrlSimpleEvent.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Csharp_Level2
+{
+    partial class ctrlSimpleEvent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(39, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trigger The Event";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ctrlSimpleEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Controls.Add(this.button1);
+            this.Name = "ctrlSimpleEvent";
+            this.Size = new System.Drawing.Size(255, 97);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
