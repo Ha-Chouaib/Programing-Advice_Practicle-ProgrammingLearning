5a63a3cf9a0ee1c35fc85bc207973a46fed8e99d
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-09-02 09:43:10 +0200

    [Data Stucture Lvel-2]-(List & LINQ)

---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/03_List/01_List/01_WorkingwithList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>(); 
+
+            Numbers.Add(1);
+            Numbers.Add(2);
+            Numbers.Add(3);
+            Numbers.Add(4);
+            Numbers.Add(5);
+
+            Console.WriteLine($"Number Of List Items: {Numbers.Count}");
+
+            Console.WriteLine($"Item 1: {Numbers[0]}");
+            Console.WriteLine($"Item 2: {Numbers[1]}");
+            Console.WriteLine($"Item 3: {Numbers[2]}");
+            Console.WriteLine($"Item 4: {Numbers[3]}");
+            Console.WriteLine($"Item 5: {Numbers[4]}");
+
+            Numbers[1] = 300;
+            Console.WriteLine("Changing Item 2's Value:" + Numbers[1]);
+
+            
+
+        }
+    }
+}
---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/03_List/03_RemoveElements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_RemoveElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine($"\nList Items: {string.Join("||", Numbers)}");
+
+            //Remove Item By Value
+            Numbers.Remove(3);
+            Console.WriteLine($"\nAfter Removing [3]->(By Value): {string.Join("||",Numbers)}");
+
+            //Remove Item By Index
+            Numbers.RemoveAt(5);
+            Console.WriteLine($"\nAfter Removing the Item In Index[5]: {string.Join("||", Numbers)}");
+
+            //Remove Items By Condition
+            Numbers.RemoveAll(n=> n <= 5);
+            Console.WriteLine($"\nAfter Removing All Items With Condition [n <= 5]): {string.Join("||", Numbers)}");
+
+            // Clear The Whole List
+            Numbers.Clear();
+            Console.WriteLine($"\nAfter Clearing the list [ List Count: { Numbers.Count}]");
+        }
+    }
+}
