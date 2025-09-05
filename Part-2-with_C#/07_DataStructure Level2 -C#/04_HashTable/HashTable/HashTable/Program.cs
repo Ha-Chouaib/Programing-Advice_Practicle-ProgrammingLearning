b8ca34e4b3e3f1b9ffbaa262466875d2b3f75f63
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-09-05 23:17:38 +0200

    [Data Structure Level-2]-(HashTable VS Dictionary)

---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/04_HashTable/HashTable/HashTable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable MyHashtable = new Hashtable();
+
+            MyHashtable.Add("Key1","Value1");
+            MyHashtable.Add("Key2","Value2");
+            MyHashtable.Add("Key3","Value3");
+
+            Console.WriteLine("Initial HashTable Contents:");
+            foreach (DictionaryEntry entry in MyHashtable)
+            {
+                Console.WriteLine($"Key: [{entry.Key}], Value: [{entry.Value}]");
+            }
+
+            //Modifying an Element
+            MyHashtable["Key1"] = "NewValue1";
+
+            //Accessing an Element
+            Console.WriteLine($"\nAccessing the [key1] Value: {MyHashtable["Key1"]}");
+
+            //Removing Element
+            MyHashtable.Remove("Key2");
+
+            //Iterating Over Elements
+            Console.WriteLine("\nCurrent HashTable Contents:");
+            foreach (DictionaryEntry entry in MyHashtable)
+            {
+                Console.WriteLine($"Key: [{entry.Key}], Value: [{entry.Value}]");
+            }
+
+        }
+    }
+}
---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/05_Dictionary/_Dictionary/02_TryGetValue_Method/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_TryGetValue_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> fruitBasket = new Dictionary<string, int> 
+            {
+                { "Apple",22},
+                { "Banana",29},
+            };
+
+            if (fruitBasket.TryGetValue("Apple", out int AppleQuantity))
+                Console.WriteLine($"Apple quantity: {AppleQuantity}");
+            else 
+                Console.WriteLine("No Apple In The Basket");
+
+            if(fruitBasket.TryGetValue("Orange", out int OrangeQuantity))
+                Console.WriteLine($"Orange quantity: {OrangeQuantity}");
+            else
+                Console.WriteLine("No Orange In The Basket");
+
+        }
+    }
+}
