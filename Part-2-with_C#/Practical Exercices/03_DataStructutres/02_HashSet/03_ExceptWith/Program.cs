5742a977950bb6a26df8c83ab801b973640ee116
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-09-15 22:24:27 +0200

    [Practical Ex]-<HashSet>

---
+++ Part-2-with_C#/Practical Exercices/03_DataStructutres/02_HashSet/03_ExceptWith/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ExceptWith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //[1]From a set of all fruits, remove fruits that are in a “favorites” set.
+            HashSet<string> allFruits = new HashSet<string>
+                            {
+                                "Apple",
+                                "Banana",
+                                "Orange",
+                                "Mango",
+                                "Grapes",
+                                "Strawberry",
+                                "Pineapple",
+                                "Watermelon"
+                            };
+                            
+           HashSet<string> favoriteFruits = new HashSet<string>
+                            {
+                                "Mango",
+                                "Strawberry",
+                                "Banana"
+                            };
+            Console.WriteLine($"[1] All Fruits :[{string.Join(" , ", allFruits)}] || Favprite Fruits:[{string.Join(" , ", favoriteFruits)}] ");
+            allFruits.ExceptWith(favoriteFruits);
+            Console.WriteLine($"=> Removed The Favorite Fruits From The AllFruits Set: {string.Join(" | ", allFruits)}");
+
+
+            //[2]From a set of numbers 1–10, remove numbers that are in another set {2, 4, 6, 8}.
+            HashSet<int> set1 = new HashSet<int> { 1, 2, 3, 4, 5,7,8,9,10 };
+            HashSet<int> set2 = new HashSet<int> { 2, 4, 6, 8 };
+
+            Console.WriteLine($"\n[2] Set1 :[{string.Join(" , ", set1)}] || Set2:[{string.Join(" , ", set2)}] ");
+            set1.ExceptWith(set2);
+            Console.WriteLine($"=> Removed The set2 Items  From The Set1: {string.Join(" | ", set1)}");
+
+            //[3]From a set of courses taken, exclude the set of courses already passed.
+
+            var coursesTaken = new HashSet<string>
+                               {
+                                   "Mathematics",
+                                   "Physics",
+                                   "Chemistry",
+                                   "Biology",
+                                   "Computer Science",
+                                   "History",
+                                   "Philosophy"
+                               };
+
+            var coursesPassed = new HashSet<string>
+                                {
+                                    "Mathematics",
+                                    "History",
+                                    "Biology"
+                                };
+
+            Console.WriteLine($"\n[3] courses Taken :[{string.Join(" , ", coursesTaken)}] || courses Passed:[{string.Join(" , ", coursesPassed)}] ");
+            coursesTaken.ExceptWith(coursesPassed);
+            Console.WriteLine($"=> Exculd The Passed courses From The courses Taken: {string.Join(" | ", coursesTaken)}");
+
+
+
+
+        }
+    }
+}
---
+++ Part-2-with_C#/Practical Exercices/03_DataStructutres/02_HashSet/04_SymmetricExceptWith/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_SymmetricExceptWith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //[1]Find elements that are unique to either Set A or Set B (but not both)
+            var setA = new HashSet<int> { 1, 2, 3, 4, 5, 6 };
+            var setB = new HashSet<int> { 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine($"[1] set A :[{string.Join(" , ", setA)}] || set B:[{string.Join(" , ", setB)}] ");
+            setA.SymmetricExceptWith(setB);
+            Console.WriteLine($"=> Unique numbers in A or B: {string.Join(" | ", setA)}");
+
+            //[2]Compare two sets of employees from two departments and find employees not in common.
+
+            var deptA = new HashSet<string> { "Ali", "Sara", "Omar", "Khalid" };
+            var deptB = new HashSet<string> { "Sara", "Khalid", "Mona", "Hana" };
+
+            Console.WriteLine($"\n[2] deptA :[{string.Join(" , ", deptA)}] || dep B:[{string.Join(" , ", deptB)}] ");
+            deptA.SymmetricExceptWith(deptB);
+            Console.WriteLine($"=>  Not In Common Employees from Dept A & B: {string.Join(" | ", deptA)}");
+
+
+            //[3]From two sets of numbers, get numbers that are present in one set but not in the other.
+            var numSet1 = new HashSet<int> { 100, 200, 300, 400 };
+            var numSet2 = new HashSet<int> { 300, 400, 500, 600 };
+
+            Console.WriteLine($"\n[3] numSet1 :[{string.Join(" , ", numSet1)}] || numSet2 :[{string.Join(" , ", numSet2)}] ");
+            numSet1.SymmetricExceptWith(numSet2);
+            Console.WriteLine($"=>  Not In Common Numbers: {string.Join(" | ", numSet1)}");
+        }
+    }
+}
