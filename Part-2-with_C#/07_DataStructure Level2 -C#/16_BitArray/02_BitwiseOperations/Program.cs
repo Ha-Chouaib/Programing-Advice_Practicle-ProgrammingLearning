918e0c6ab71ac4544402abd0e14fe492951865a6
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-09-19 21:50:59 +0200

    [Data Structuers]-<BitArray & JaggedArray>

---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/16_BitArray/02_BitwiseOperations/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BitwiseOperations
+{
+    internal class Program
+    {
+        static string BitArrayToString(BitArray bitArr)
+        {
+            char[] chars = new char[bitArr.Length];
+            for (int i = 0; i < bitArr.Length; i++)
+            {
+                chars[i] = bitArr[i] ? '1' : '0';
+
+            }
+            return new string(chars);
+        }
+        static void Main(string[] args)
+        {
+
+            // Create two BitArrays
+            BitArray bits1 = new BitArray(new bool[] { true, false, true, false });
+            BitArray bits2 = new BitArray(new bool[] { true, true, true, false });
+
+
+            Console.WriteLine("bits1 : " + BitArrayToString(bits1));
+            Console.WriteLine("bits2 : " + BitArrayToString(bits2));
+            Console.WriteLine("BitWise Operators:");
+
+
+            // Bitwise AND operation
+            BitArray resultAnd = new BitArray(bits1);
+            resultAnd.And(bits2);
+
+
+            Console.WriteLine("\nBitwise AND result: ");
+            Console.WriteLine(BitArrayToString(bits1));
+            Console.WriteLine(BitArrayToString(bits2));
+            Console.WriteLine("------------");
+            Console.WriteLine(BitArrayToString(resultAnd));
+
+            // Bitwise Or operation
+
+            BitArray resultOr = new BitArray(bits1);
+            resultOr.Or(bits2);
+
+
+            Console.WriteLine("\nBitwise Or result: ");
+            Console.WriteLine(BitArrayToString(bits1));
+            Console.WriteLine(BitArrayToString(bits2));
+            Console.WriteLine("------------");
+            Console.WriteLine(BitArrayToString(resultOr));
+
+            // Bitwise Not operation
+            BitArray resultNot = new BitArray(bits1);
+            resultNot.Not();
+
+            Console.WriteLine("\nBitwise Not result: ");
+            Console.WriteLine(BitArrayToString(bits1));
+            Console.WriteLine("------------");
+            Console.WriteLine(BitArrayToString(resultNot));
+
+            // Bitwise Xor operation
+            /*
+             Explaining the XOR:
+             XOR Table: returns true of the two bits are diffrent, otherwise returns false.
+
+                | A | B | A XOR B |
+                |---|---|---------|
+                | 0 | 0 |    0    |
+                | 0 | 1 |    1    |
+                | 1 | 0 |    1    |
+                | 1 | 1 |    0    |
+             */
+            BitArray resultXor = new BitArray(bits1);
+            resultXor.Xor(bits2);
+
+
+            Console.WriteLine("\nBitwise Xor result: ");
+            Console.WriteLine(BitArrayToString(bits1));
+            Console.WriteLine(BitArrayToString(bits2));
+            Console.WriteLine("------------");
+            Console.WriteLine(BitArrayToString(resultXor));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Part-2-with_C#/07_DataStructure Level2 -C#/17_JaggedArray/02_LINQJaggedArrayExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_LINQJaggedArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialize a jagged array
+            int[][] jaggedArray = {
+            new int[] { 1, 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9, 10 }
+        };
+
+
+            // Flatten the jagged array and sum all elements
+            int totalSum = jaggedArray.SelectMany(subArray => subArray).Sum();
+            Console.WriteLine("Total Sum: " + totalSum);
+
+
+            // Find the maximum element in the jagged array
+            int maxElement = jaggedArray.SelectMany(subArray => subArray).Max();
+            Console.WriteLine("Maximum Element: " + maxElement);
+
+
+            // Filter arrays having more than 3 elements and select their first element
+            var firstElements = jaggedArray.Where(subArray => subArray.Length > 3)
+                                           .Select(subArray => subArray.First());
+            Console.Write("First Elements of Long Rows: ");
+            foreach (var element in firstElements)
+            {
+                Console.Write(element + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
