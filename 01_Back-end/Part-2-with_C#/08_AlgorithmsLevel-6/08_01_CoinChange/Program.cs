c0d39ca12ed274b039b2684333d25d8992545858
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-11-19 21:59:32 +0100

    [Problem Solving Level-6]-<Coin Change,Activity Selection,Browser Back Button, Convert Decimal To Binary, Undo Operations in Calculator >Problems

--- 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/08_01_CoinChange/Program.cs
+++ 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/08_01_CoinChange/Program.cs
@@ -8,8 +8,31 @@ namespace _08_01_CoinChange
 {
     internal class Program
     {
+        static List<int> MinCoinsNeeded(List<int> Denominations, int Amount)
+        {
+
+            List<int> result = new List<int>();
+
+            foreach (var coin in Denominations.OrderByDescending(n => n))
+            {
+                while (Amount >= coin)
+                {
+                    result.Add(coin);
+                    Amount -= coin;
+                }
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
+            List<int> DollarDenomination = new List<int> { 1, 5, 10, 20, 50, 100 };
+            int Amount = 30;
+
+            Console.WriteLine($"The minimum number of coins needed to make up that amount [{Amount}] using the largest denominations possible:");
+            foreach (int coin in MinCoinsNeeded(DollarDenomination, Amount))
+            {
+                Console.WriteLine(coin);
+            }
         }
     }
 }
