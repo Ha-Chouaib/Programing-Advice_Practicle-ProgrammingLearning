0a8294d30596137aac2133379584f866e5fe16b5
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-11-28 07:30:09 +0100

    [Html]-<Picture Element>

--- 01_Back-end/Part-2-with_C#/07_DataStructure Level2 -C#/27_Graphs/02_Implementation - List/obj/Release/net8.0/GraphImplementation - List.AssemblyInfo.cs
+++ 01_Back-end/Part-2-with_C#/07_DataStructure Level2 -C#/27_Graphs/02_Implementation - List/obj/Release/net8.0/GraphImplementation - List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphImplementation - List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a637caddd08872aab8746254114e097bf755d81c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9761064d4136d84454d76c11d111c62054384b44")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphImplementation - List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphImplementation - List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
