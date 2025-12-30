323391d88e5c6a8141dfdc66552148bd75e53a7b
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2025-12-30 08:01:03 +0100

    [ Front-end -> CSS]-<CSS Inheritance && Colors>

--- 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/09_21_ImplementPriorityQueue/Program.cs
+++ 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/09_21_ImplementPriorityQueue/Program.cs
@@ -8,8 +8,55 @@ namespace _09_21_ImplementPriorityQueue
 {
     internal class Program
     {
+        public class clsPriorityQueue
+        {
+            private SortedList<int, int> _ListItems= new SortedList<int, int>();
+             public void Enqueue(int priority ,int Item)
+            {   if(!_ListItems.ContainsKey(priority))               
+                    _ListItems.Add(priority, Item);
+            }
+
+            public (int? Priority, int? value) Dequeue()
+            {
+                if (_ListItems.Count == 0)  return (null,null);
+
+                var Item = _ListItems.First();
+                _ListItems.Remove(Item.Key);
+                return (Item.Key, Item.Value);
+            }
+            public int Count()
+            {
+                return _ListItems.Count; 
+            }
+            public (int priority,int value) Peek()
+            {
+                var item = _ListItems.First();
+                return (item.Key,item.Value);
+            }
+
+            public IEnumerator<KeyValuePair<int, int>> GetEnumerator()
+            {
+                foreach (var item in _ListItems)
+                {
+                    yield return item;
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
+            clsPriorityQueue myPriorityQueue = new clsPriorityQueue();
+            myPriorityQueue.Enqueue(10, 1);
+            myPriorityQueue.Enqueue(5, 2);
+            myPriorityQueue.Enqueue(20, 3);
+            Console.WriteLine("Peek: " + myPriorityQueue.Peek());
+            while (myPriorityQueue.Count() > 0)
+            {
+                    var item = myPriorityQueue.Dequeue();
+                    Console.WriteLine("Priority: " + item.Priority + " Item: " + item.value);
+                    
+              
+            }
         }
     }
 }
