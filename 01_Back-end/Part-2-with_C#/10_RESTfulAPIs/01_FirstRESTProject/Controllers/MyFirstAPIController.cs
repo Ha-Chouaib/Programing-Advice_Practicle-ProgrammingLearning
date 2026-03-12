1f941dc0b93585a0c273c571bb1dc3fc281d8c74
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-12 17:12:52 +0100

    [RESTful API]->(First Project)

---
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/01_FirstRESTProject/Controllers/MyFirstAPIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_FirstRESTProject.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/MyFirstAPI")]
+    [ApiController]
+    public class MyFirstAPIController : ControllerBase
+    {
+        [HttpGet ("MyName",Name ="MyName")]
+        public string GetMyName()
+        {
+            return "My Name Is Chouaib Hadadi";
+        }
+        
+        [HttpGet ("YourName",Name ="YourName")]
+        public string GetyourName()
+        {
+            return "Your Name Is Mohammed Abu-Hadhoud";
+        }
+
+        [HttpGet("sum/{n1}/{n2}")]
+        public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        [HttpGet ("multiply/{n1}/{n2}")]
+        public int Multipy(int n1, int n2) { return n1 * n2; }
+    }
+}
