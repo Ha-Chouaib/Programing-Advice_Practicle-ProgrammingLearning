326c362f2408a376cedd4b1505353b2f35095c42
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-14 18:17:19 +0100

    [RESTful APIs]-(Student API project)-<GetAll() Get(PassedStudents) GetAverageGrade()>

--- 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/01_FirstRESTProject/obj/Debug/net8.0/01_FirstRESTProject.AssemblyInfo.cs
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/01_FirstRESTProject/obj/Debug/net8.0/01_FirstRESTProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_FirstRESTProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ed7d035a17f771f1bbb8a31a0b8184306b68eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df33ed9390f37c572cc36dc72b3bcbecfb06265f")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_FirstRESTProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_FirstRESTProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/02_StudentAPI_Project_GetAll/DataSimulation/StudentData.cs
@@ -0,0 +1,17 @@
+﻿using _02_StudentsAPI.Models;
+
+namespace _02_StudentsAPI.DataSimulation
+{
+    public class StudentData
+    {
+
+        public static readonly List<Student> StudentsList = new List<Student>
+        {
+            new Student{Id = 1, Name ="Ahmed",Age= 30, Grade =30},
+            new Student{Id = 2, Name ="Hind",Age= 22, Grade = 70},
+            new Student{Id = 3, Name ="Mobarak",Age= 19, Grade =90},
+            new Student{Id = 4, Name ="Lahsen",Age= 26, Grade =10},
+            new Student{Id = 5, Name ="Kamal",Age=23, Grade =80}
+        };
+    }
+}
