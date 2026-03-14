2fc83fd2b14a8bd33e8a6689591d46c42dcb4dad
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-14 18:17:19 +0100

    [RESTful APIs]-(Student API project)-<GetAll() Get(PassedStudents) GetAverageGrade()>

---
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/02_StudentAPI_Project_GetAll/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using _02_StudentsAPI.DataSimulation;
+using _02_StudentsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_StudentsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Students")]
+    public class StudentsController : Controller
+    {
+
+        [HttpGet("All",Name = "GetAllStudents")]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            return Ok(StudentData.StudentsList);
+        }
+        [HttpGet("Passed", Name = "GetPassedStudents")]
+        public ActionResult<IEnumerable<Student>> GetPassedStudents()
+        {
+            return Ok(StudentData.StudentsList.Where(s => s.Grade >= 50).ToList());
+        }
+
+
+        [HttpGet ("AVG", Name = "GetAverageGrade")]
+        public ActionResult<double> GetAverageGrade()
+        {
+            //StudentData.StudentsList.Clear();
+            if (StudentData.StudentsList.Count == 0) return NotFound("No Students Data Found!");
+            double avg = StudentData.StudentsList.Average(s => s.Grade);
+            return Ok(avg);
+        }
+    }
+}
---
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/RESTfulAPI_ClientSideCode/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Http.Json;
+
+namespace RESTfulAPI_ClientSideCode
+{
+     class Program
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+       
+
+
+        static async Task Main(string[] args)
+        {
+            httpClient.BaseAddress = new Uri("https://localhost:7010/api/Students/");
+           
+
+            await GetAllStudents();
+            await  GetPassedStudents();
+            await GetAverageGrade();
+        }
+
+        static async Task GetAllStudents()
+        {
+
+            try
+            {
+                Console.WriteLine("\n__________________________________________________");
+                Console.WriteLine("\nFetching All Students...\n");
+                var Students = await httpClient.GetFromJsonAsync<List<Student>>("All");
+
+                if (Students != null)
+                {
+                    foreach (var s in Students) Console.WriteLine($"ID:{s.Id} | Name:{s.Name} | Age:{s.Age} | Grade:{s.Grade}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static async Task GetPassedStudents()
+        {
+            try
+            {
+                Console.WriteLine("\n\n__________________________________________________");
+                Console.WriteLine("\nFetching Passed Students...\n");
+                var Students = await httpClient.GetFromJsonAsync<List<Student>>("Passed");
+
+                if (Students != null)
+                {
+                    foreach (var s in Students) Console.WriteLine($"ID:{s.Id} | Name:{s.Name} | Age:{s.Age} | Grade:{s.Grade}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    
+        static async Task GetAverageGrade()
+        {
+            try
+            {
+                Console.WriteLine("\n\n__________________________________________________");
+                Console.WriteLine("\nFetching AVerage Grade.\n");
+                var AVG = await httpClient.GetFromJsonAsync<List<Student>>("AVG");
+
+                Console.WriteLine($"Average Grade: {AVG}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            } 
+        }
+    }
+}
