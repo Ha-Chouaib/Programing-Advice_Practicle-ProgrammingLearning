38564ea21aa4fecbeb81ce5b92c8e6235e7fc6c3
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-31 00:02:06 +0200

    [SecureAPI]<StudentAPI project>-(Foundation Level Done -HTTPS&&CORS)

--- 01_Back-end/Part-2-with_C#/11_Secure APIs_JWT/01 StudentAPIServer v7/MyFirstRestAPI_Porject/obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
+++ 01_Back-end/Part-2-with_C#/11_Secure APIs_JWT/01 StudentAPIServer v7/MyFirstRestAPI_Porject/obj/Debug/net8.0/StudentApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734f836a0cc9bb171de8c2ad4420bcc2adf7187f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b25608068da892529f7f093eb4501ff82788a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
