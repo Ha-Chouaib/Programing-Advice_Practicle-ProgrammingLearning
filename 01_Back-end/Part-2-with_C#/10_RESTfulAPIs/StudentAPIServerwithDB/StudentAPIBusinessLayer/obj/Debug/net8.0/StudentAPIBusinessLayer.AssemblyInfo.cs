5f3b4c909497740a6f317cfb9f71c6c99e0bd074
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-30 22:16:01 +0200

    [SecureAPI]-(Encoding | Encryption | Hashing Demos)

--- 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/StudentAPIServerwithDB/StudentAPIBusinessLayer/obj/Debug/net8.0/StudentAPIBusinessLayer.AssemblyInfo.cs
+++ 01_Back-end/Part-2-with_C#/10_RESTfulAPIs/StudentAPIServerwithDB/StudentAPIBusinessLayer/obj/Debug/net8.0/StudentAPIBusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPIBusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734f836a0cc9bb171de8c2ad4420bcc2adf7187f")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPIBusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPIBusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
