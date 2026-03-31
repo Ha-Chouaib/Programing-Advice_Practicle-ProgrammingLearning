849d8d59e66a43da982e76f3d9ef3035d16bf77c
Ha-Chouaib <Chouaib.Hadadi01@gmail.com>
2026-03-31 16:14:35 +0200

    [SecureAPI]-<StudentAPI project>-(Prepare Authentication setups)

---
+++ 01_Back-end/Part-2-with_C#/11_Secure APIs_JWT/StudentAPIServerwithDB/StudentAPIBusinessLayer/clsCryptography.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPIBusinessLayer
+{
+    public sealed class clsCryptography
+    {
+        public static string PassowrdBcyptHashing(string input)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(input);
+        }
+    }
+}
