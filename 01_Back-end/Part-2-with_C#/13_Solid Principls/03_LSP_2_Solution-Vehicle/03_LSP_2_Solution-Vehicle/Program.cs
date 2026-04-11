d52d46065fb61814d5478a722bbc85f76c9174de
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2026-04-11 16:48:19 +0200

    [Solid Priciples]-(Liskov Substruction Principle <LSP> Done)

---
+++ 01_Back-end/Part-2-with_C#/13_Solid Principls/03_LSP_2_Solution-Vehicle/03_LSP_2_Solution-Vehicle/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _03_LSP_2_Solution_Vehicle
+{
+    public class Vehicle
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("Driving...");
+        }
+
+    }
+
+    public class EngineVehivle : Vehicle
+    {
+        public virtual void StartEngine()
+        {
+            Console.WriteLine("Starting engine...");
+        }
+    }
+    public class Car : EngineVehivle
+    {
+        public override void StartEngine()
+        {
+            Console.WriteLine("Car engine started...");
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine("Car is driving...");
+        }
+    }
+
+    public class Bicycle : Vehicle
+    {
+      
+        public override void Drive()
+        {
+            Console.WriteLine("Bicycle is riding...");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EngineVehivle vehicle1 = new Car();
+            vehicle1.StartEngine();
+            vehicle1.Drive();
+
+            Vehicle vehicle2 = new Bicycle();
+          
+            vehicle2.Drive();        // Behaves as expected for a bicycle.
+        }
+    }
+
+}
