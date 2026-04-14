d58343e5c500faed2e5754ffd88fc76f9c4b61ae
Ha-Chouaib <chouaib.hadadi01@gmail.com>
2026-04-14 19:39:46 +0200

    [Algorithms Level 6]-(Basic Calculator) Done

--- 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/09_29_BasicCalculator/Program.cs
+++ 01_Back-end/Part-2-with_C#/08_AlgorithmsLevel-6/09_29_BasicCalculator/Program.cs
@@ -8,96 +8,63 @@ namespace _09_29_BasicCalculator
 {
     internal class Program
     {
-        static int DoCalc(int a, int b ,char Operator)
-        {
-            switch(Operator)
-            {
-                case '+':
-                    return a + b;
-                case '-':
-                    return a - b;
-                default:
-                    return 0;
-            }
-        }
-        static int Calculate(string input)
-        {
-            List<char> parentheses = new List<char> { '(',')'};
-            List<char> Operators = new List<char> { '+','-'};
-
-            Stack<int> numbers = new Stack<int>();
-            Queue<int> PriorityNumbers = new Queue<int>();
-            Stack<char> operations = new Stack<char>();
-            int i = 0;
-            bool HasPriority = false;
-            char value;
-            char op = new char();
+        //[problem]
+        /*
             
-            while (i < input.Length)
+            Problem: Evaluate a mathematical expression containing +, -, (, ) without * or /.
+            
+            Example:
+            Input: "1 + (2 - 3)"
+            Output: 0
+         */
+       
+        static int calculate(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            int result = 0, sign = 1, number = 0;
+
+
+            foreach (char c in s)
             {
-                value = input[i];
-                
-                if (parentheses.Contains(value) || (char.IsDigit(value) && HasPriority) || (Operators.Contains(value) && HasPriority))
+                if (char.IsDigit(c))
                 {
-                    if (value == '(') HasPriority = true;                  
-                    if (value == ')') HasPriority = false;
-                    
-                    if (HasPriority )
-                    {
-                        if (char.IsDigit(value))
-                        {
-                            int b = value - '0';
-                            int a = 0;
-                            if (numbers.Count > 0)
-                            {
-                                a = numbers.Pop();
-                            }
-                            int result = DoCalc(a, b, op);
-                            numbers.Push(result);
-                        }
-                        if (Operators.Contains(value))
-                        {
-                            op = value;
-                        }
-                       
-                    }
-                    i++;
-
+                    number = number * 10 + (c - '0');
                 }
-
-
-                if (!HasPriority)
+                else if (c == '+')
                 {
-                    if (Operators.Contains(value))
-                    {
-                        op = value;
-                        i++;
-                    }
-                    if (char.IsDigit(value))
-                    {
-                        int b = value - '0';
-                        int a = 0;
-                        if (numbers.Count > 0)
-                        {
-                            a = numbers.Pop();
-                        }
-                        
-                        int result = DoCalc(a, b, op);
-                        numbers.Push(result);
-                        i++;
-                    }
-
-
+                    result += sign * number;
+                    number = 0;
+                    sign = 1;
                 }
-                
+                else if (c == '-')
+                {
+                    result += sign * number;
+                    number = 0;
+                    sign = -1;
+                }
+                else if (c == '(')
+                {
+                    stack.Push(result);
+                    stack.Push(sign);
+                    result = 0;
+                    sign = 1;
+                }
+                else if (c == ')')
+                {
+                    result += sign * number;
+                    number = 0;
+                    result *= stack.Pop();
+                    result += stack.Pop();
+                }
             }
-            return numbers.Pop();
 
+            result += sign * number;
+            return result;
         }
         static void Main(string[] args)
         {
-            string input = "1+(1+2)";
-            int result = Calculate(input);
+            string input = "1 - (2+(1+2))";
+            int result = calculate(input);
             Console.WriteLine("input: " + input);
             Console.WriteLine("Result: " + result);
             Console.ReadLine();
